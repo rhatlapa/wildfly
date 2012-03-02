d72f9900e14eb4503a22418b96f1d7fe581ad676
Radim Hatlapatka <rhatlapa@redhat.com>
2012-03-02 09:51:43 +0100

    Descriptor testing of ejb-spec vs jboss-spec till tests of interceptors

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/descriptor/configuration/MDBTestCase.java
@@ -0,0 +1,107 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jboss.as.test.integration.ejb.descriptor.configuration;
+
+import java.util.Date;
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.jms.TextMessage;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.test.integration.common.jms.JMSOperations;
+import org.jboss.as.test.integration.common.jms.JMSOperationsProvider;
+import org.jboss.as.test.integration.ejb.mdb.DDBasedMDB;
+import org.jboss.as.test.integration.ejb.mdb.JMSMessagingUtil;
+import org.jboss.logging.Logger;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author rhatlapa
+ */
+@RunWith(Arquillian.class)
+@ServerSetup({org.jboss.as.test.integration.ejb.descriptor.configuration.MDBTestCase.JmsQueueSetup.class})
+public class MDBTestCase {
+    
+    private static final Logger logger = Logger.getLogger(MDBTestCase.class);
+
+    static class JmsQueueSetup implements ServerSetupTask {
+
+        private JMSOperations jmsAdminOperations;
+
+        @Override
+        public void setup(ManagementClient managementClient, String containerId) throws Exception {
+            jmsAdminOperations = JMSOperationsProvider.getInstance(managementClient);
+            jmsAdminOperations.createJmsQueue("mdbtest/queue", "java:jboss/mdbtest/queue");
+            jmsAdminOperations.createJmsQueue("mdbtest/replyQueue", "java:jboss/mdbtest/replyQueue");
+            jmsAdminOperations.createJmsQueue("mdbtest/redefinedQueue", "java:jboss/mdbtest/redefinedQueue");
+        }
+
+        @Override
+        public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
+            if (jmsAdminOperations != null) {
+                jmsAdminOperations.removeJmsQueue("mdbtest/queue");
+                jmsAdminOperations.removeJmsQueue("mdbtest/replyQueue");
+                jmsAdminOperations.removeJmsQueue("mdbtest/redefinedQueue");
+                jmsAdminOperations.close();
+            }
+        }
+    }
+
+    @Deployment
+    public static Archive getDeployment() {
+
+        final JavaArchive ejbJar = ShrinkWrap.create(JavaArchive.class, "mdb.jar");
+        ejbJar.addPackage(SimpleMessageDrivenBean.class.getPackage());
+        ejbJar.addPackage(JMSOperations.class.getPackage());
+        ejbJar.addClass(org.jboss.as.test.integration.ejb.mdb.JMSMessagingUtil.class);
+        ejbJar.addClass(org.jboss.as.test.integration.ejb.descriptor.configuration.MDBTestCase.JmsQueueSetup.class);
+        ejbJar.addAsManifestResource(MDBTestCase.class.getPackage(), "ejb-jar.xml", "ejb-jar.xml");
+        ejbJar.addAsManifestResource(MDBTestCase.class.getPackage(), "jboss-ejb3.xml", "jboss-ejb3.xml");
+        ejbJar.addAsManifestResource(new StringAsset("Dependencies: org.jboss.as.controller-client, org.jboss.dmr \n"), "MANIFEST.MF");
+        return ejbJar;
+    }
+    
+    @EJB(mappedName = "java:module/JMSMessagingUtil")
+    private JMSMessagingUtil util;
+    
+    @Resource(mappedName = "java:jboss/mdbtest/queue")
+    private Queue queue;
+    
+    @Resource(mappedName = "java:jboss/mdbtest/redefinedQueue")
+    private Queue redefinedQueue;
+    
+    @Resource(mappedName = "java:jboss/mdbtest/replyQueue")
+    private Queue replyQueue;
+
+    /**
+     * Test a deployment descriptor based MDB
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testMDB() throws Exception {
+        
+        this.util.sendTextMessage("Hello ejb", this.queue, replyQueue);
+        this.util.sendTextMessage("Hello jboss-spec", this.redefinedQueue, replyQueue);
+        logger.info("Start time of waiting for message in MDB test: " + new Date().getTime());
+        final Message reply = this.util.receiveMessage(replyQueue, 5000);             
+        logger.info("End time of waiting for message in MDB test: " + new Date().getTime());
+        Assert.assertNotNull("Reply message was null on reply queue: " + this.replyQueue, reply);
+        final String result = ((TextMessage) reply).getText();
+        Assert.assertEquals("MDB should listen on redefinedQueue","replying Hello jboss-spec", result);
+    }
+}
---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/descriptor/configuration/SimpleBeanTestCase.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jboss.as.test.integration.ejb.descriptor.configuration;
+
+import javax.naming.InitialContext;
+import javax.naming.NameNotFoundException;
+import javax.naming.NamingException;
+import junit.framework.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.fail;
+
+/**
+ *
+ * @author rhatlapa
+ */
+@RunWith(Arquillian.class)
+public class SimpleBeanTestCase {
+    @Deployment
+    public static Archive<?> deployment() {
+        final JavaArchive jar = ShrinkWrap.create(JavaArchive.class, 
+                "ejb-descriptor-configuration-test.jar")
+                .addPackage(SessionTypeSpecifiedBean.class.getPackage())
+                .addAsManifestResource(SimpleBeanTestCase.class.getPackage(), "jboss-ejb3.xml", "jboss-ejb3.xml")
+                .addAsManifestResource(SimpleBeanTestCase.class.getPackage(), "ejb-jar.xml", "ejb-jar.xml");
+        return jar;
+    }
+
+
+    @Test
+    public void testSimpleBeanStateless() throws NamingException {
+        final InitialContext ctx = new InitialContext();
+        try {
+            final SessionTypeSpecifiedBean bean = (SessionTypeSpecifiedBean) ctx.lookup("java:module/simpleBeanDefinitionUnknown");
+            fail("The SimpleBean should not be available");
+        } catch (NameNotFoundException e) {
+            // good
+        }
+    }
+
+    @Test
+    public void testSimpleBeanSingleton() throws NamingException {
+        final InitialContext ctx = new InitialContext();
+        SessionTypeSpecifiedBean bean = (SessionTypeSpecifiedBean) ctx.lookup("java:module/simpleBeanDefinition");
+
+        bean.setName("Singleton");
+        bean = (SessionTypeSpecifiedBean) ctx.lookup("java:module/simpleBeanDefinition");
+
+        Assert.assertEquals("As singleton the name should remained set", "Hi Singleton", bean.greet());
+
+    }
+
+    @Test
+    public void testBeanInjection() throws NamingException {
+        final InitialContext ctx = new InitialContext();
+
+        final SimpleInjectionBeanInterface bean = (SimpleInjectionBeanInterface) ctx.lookup("java:module/simpleBeanWithInjection");
+        try {
+            bean.checkInjection();
+            Assert.assertEquals("Bean wasn't redefined by JBoss specific descriptor to use SimpleSessionBean", 
+                    "Redefined Greetings", bean.greetInjectedBean("InjectionTest"));
+        } catch (RuntimeException e) {
+            fail("Bean is not correctly injected");
+
+        }
+    }
+    
+    @Test
+    public void testInterceptor() throws NamingException {
+        final InitialContext ctx = new InitialContext();
+        final SimpleHelloBean helloBean = (SimpleHelloBean) ctx.lookup("java:module/simpleHelloBean");
+        Assert.assertEquals("Interception method wasn't changed by jboss spec descriptor","Hello JbossSpecInterceptedHelloBean", helloBean.hello("HelloBean"));
+    }
+}
